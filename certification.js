c7871a7aaa38f979e116d064c1d885c753002f32
Deepthichittajallu <chlakshmideepthi06@gmail.com>
2025-09-10 23:51:32 +0530

    media queries yet to be applied

--- certification.js
+++ certification.js
@@ -10,156 +10,140 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-const tools = [
-  {
-    "title":"HTML",
-  }
-]
-const skillsData = {
-    coding: [
-      {
-        title: 'Leetcode',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/c/c2/LeetCode_Logo_2.png',
-        description: ['Solved 400+ problems', 'Highest rating of 1461'],
-        link: 'https://leetcode.com/u/Deepthi_Chittajallu/',
-        btnText: 'View Profile'
-      },
-      {
-        title: 'Codechef',
-        image: 'https://cdn.codechef.com/sites/all/themes/abessive/cc-logo.png',
-        description: ['Solved 1100+ problems', 'Highest rating of 1423'],
-        link: 'https://www.codechef.com/users/deepthi2403',
-        btnText: 'View Profile'
-      },
-      {
-        title: 'HackerRank',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/6/65/HackerRank_logo.png',
-        description: ['Achieved 7 Badges', '2+ Certifications'],
-        link: 'https://www.hackerrank.com/profile/chlakshmideepth1',
-        btnText: 'View Profile'
-      },
-      {
-        title: 'Geeks for Geeks',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/e/eb/GeeksForGeeks_logo.png',
-        description: ['Solved 200+ problems', 'Highest rating of 1724'],
-        link: 'https://www.geeksforgeeks.org/user/chlakshmidzjpq/',
-        btnText: 'View Profile'
-      }
-    ],
-    frontend: [
-      {
-        title: 'HTML',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEc9A_S6BPxCDRp5WjMFEfXrpCu1ya2OO-Lw&s',
-        description: [],
-        link: 'https://www.geeksforgeeks.org/user/chlakshmidzjpq/',
-        btnText: 'View Certificate'
-      },
-      {
-        title: 'CSS',
-        image: './assets/css.png',
-        description: [],
-        link: 'https://www.geeksforgeeks.org/user/chlakshmidzjpq/',
-        btnText: 'View Certificate'
-      },
-      {
-        title: 'JavaScript',
-        image: './assets/js.png',
-        description: [],
-        link: 'https://www.geeksforgeeks.org/user/chlakshmidzjpq/',
-        btnText: 'View Certificate'
-      },
-      {
-        title: 'Bootstrap',
-        image: './assets/bootstarp.png',
-        description: [],
-        link: 'https://www.geeksforgeeks.org/user/chlakshmidzjpq/',
-        btnText: 'View Certificate'
-      },
-      {
-        title: 'React',
-        image: './assets/react.png',
-        description: [],
-        link: 'https://www.geeksforgeeks.org/user/chlakshmidzjpq/',
-        btnText: 'View Certificate'
-      }
-    ],
-    database: [
-      {
-        title: 'MySQL',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Sql_data_base_with_logo.png/640px-Sql_data_base_with_logo.png',
-        description: ['Experienced in joins, indexing, stored procedures'],
-        link: '#',
-        btnText: 'View Certificate'
-      }
-    ],
-    tools: [
-      {
-        title: 'Git & GitHub',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0TLkHZgGiaukgrwZo0niTUPvNRxganER5AA&s',
-        description: ['Version control', 'Collaboration workflows'],
-        link: 'https://github.com/Deepthichittajallu',
-        btnText: 'View Profile'
-      },
-      {
-        title: 'Linux & Redhat',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVm-8cHPLKhsqrjwWSAkhVsImyiXlf08_TEg&s',
-        description: ['Linux', 'Explored different OS'],
-        link: '#',
-        btnText: 'View Certificate'
-      }
-    ]
-  };
-  function renderSkills(category) {
-      const contentContainer = document.getElementById('skills-content');
-    contentContainer.innerHTML = '';
-    const skillCards = skillsData[category];
-    if (!skillCards || skillCards.length === 0) {
-      contentContainer.innerHTML = `<p style="color: white;">No data available for "${category}"</p>`;
-      return;
+
+const certData = {
+  certifications: [
+    {
+      title: 'Oracle',
+      image: './assets/oracle.png',
+      description: ['Issued by Oracle Academy', 'Completed in 2025'],
+      link: './assets/oracle.pdf',
+      btnText: 'View Certificate'
+    },
+    {
+      title: 'Redhat',
+      image: './assets/redhat(ss).png',
+      description: ['Issued by Oracle Academy', 'Completed in 2025'],
+      link: './assets/Redhat.pdf',
+      btnText: 'View Certificate'
+    },
+    {
+      title: 'HTML',
+      image: './assets/html(cs).png',
+      description: ['Issued by Cisco', 'Completed in 2025'],
+      link: 'https://www.credly.com/badges/8edb0a9e-9e7d-46fb-9b64-9b6da809919f/public_url',
+      btnText: 'View Certificate'
+    },
+    {
+      title: 'CSS',
+      image: './assets/css(cs).png',
+      description: ['Issued by Cisco', 'Completed in 2025'],
+      link: 'https://www.credly.com/badges/c770a91d-2b74-4a0a-b089-2ba3b70c0ed9/public_url',
+      btnText: 'View Certificate'
+    },
+    {
+      title: 'Javascript',
+      image: './assets/javas.png',
+      description: ['Issued by Cisco', 'Completed in 2025'],
+      link: 'https://www.credly.com/badges/15996165-2d95-45f5-82f8-8eceba81f508/public_url',
+      btnText: 'View Certificate'
+    },
+  ],
+  global: [
+    {
+      title: 'Oracle',
+      image: './assets/oracle.png',
+      description: ['Issued by Oracle Academy', 'Completed in 2025'],
+      link: './assets/oracle.pdf',
+      btnText: 'View Certificate'
+    },
+    {
+      title: 'Redhat',
+      image: './assets/redhat(ss).png',
+      description: ['Issued by Oracle Academy', 'Completed in 2025'],
+      link: './assets/Redhat.pdf',
+      btnText: 'View Certificate'
     }
+  ],
+  badges: [
+    {
+      title: 'HTML',
+      image: './assets/html(cs).png',
+      description: ['Issued by Cisco', 'Completed in 2025'],
+      link: 'https://www.credly.com/badges/8edb0a9e-9e7d-46fb-9b64-9b6da809919f/public_url',
+      btnText: 'View Certificate'
+    },
+    {
+      title: 'CSS',
+      image: './assets/css(cs).png',
+      description: ['Issued by Cisco', 'Completed in 2025'],
+      link: 'https://www.credly.com/badges/c770a91d-2b74-4a0a-b089-2ba3b70c0ed9/public_url',
+      btnText: 'View Certificate'
+    },
+    {
+      title: 'Javascript',
+      image: './assets/javas.png',
+      description: ['Issued by Cisco', 'Completed in 2025'],
+      link: 'https://www.credly.com/badges/15996165-2d95-45f5-82f8-8eceba81f508/public_url',
+      btnText: 'View Certificate'
+    },
+    {
+      title: 'Python',
+      image: './assets/python.png',
+      description: ['Issued by Cicco', 'Completed in 2024'],
+      link: 'https://www.credly.com/badges/9b7ff53f-42bd-4c36-8b2e-b95a44bff569/public_url',
+      btnText: 'View Certificate'
+    },
+    {
+      title: 'OS',
+      image: './assets/os.png',
+      description: ['Issued by Cicco', 'Completed in 2024'],
+      link: 'https://www.credly.com/badges/11cc48df-1230-44b6-be61-58d8e2ca2798/public_url',
+      btnText: 'View Certificate'
+    }
+  ]
+};
 
-    skillCards.forEach(skill => {
-      const card = document.createElement('div');
-      card.className = 'skill-card';
+function renderCerts(category) {
+  const container = document.getElementById('certifications-content');
+  container.innerHTML = '';
 
-      card.innerHTML = `
-        <img src="${skill.image}" class="code-img"/>
-        <div class="overlay">
-          <div class="card-content">
-            <h3>${skill.title}</h3>
-            ${skill.description.map(line => `<p>${line}</p>`).join('')}
-            <button class="code-btn">
-              <a href="${skill.link}" class="code-btn-link" target="_blank">${skill.btnText}</a>
-            </button>
-          </div>
-        </div>
-      `;
-
-      contentContainer.appendChild(card);
-    });
+  const certs = certData[category];
+  if (!certs || certs.length === 0) {
+    container.innerHTML = `<p style="color: white;">No certifications found for "${category}"</p>`;
+    return;
   }
-document.addEventListener('DOMContentLoaded', () => {
-  renderSkills('coding');
-  document.querySelectorAll('.skill-box a').forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-      const selected = this.getAttribute('data-skill');
-      renderSkills(selected);
-      document.querySelectorAll('.skill-box a').forEach(el => el.classList.remove('active'));
-      this.classList.add('active');
-    });
+
+  certs.forEach(cert => {
+    const card = document.createElement('div');
+    card.className = 'skill-card';
+
+    card.innerHTML = `
+      <img src="${cert.image}" class="code-img" />
+      <div class="overlay">
+        <div class="card-content">
+          <h3>${cert.title}</h3>
+          ${cert.description.map(line => `<p>${line}</p>`).join('')}
+          <button class="code-btn">
+            <a href="${cert.link}" class="code-btn-link" target="_blank">${cert.btnText}</a>
+          </button>
+        </div>
+      </div>
+    `;
+    container.appendChild(card);
   });
-});
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  renderSkills('coding');
+  renderCerts('certifications');
+
   document.querySelectorAll('.skill-box a').forEach(link => {
     link.addEventListener('click', function (e) {
       e.preventDefault();
       document.querySelectorAll('.skill-box a').forEach(el => el.classList.remove('active'));
       this.classList.add('active');
-      const selected = this.getAttribute('data-skill');
-      renderSkills(selected);
+      const selected = this.getAttribute('data-cert');
+      renderCerts(selected);
     });
   });
 });
